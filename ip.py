ce0723156716d646fe6f130cbd75576fb360c855
prolin99 <prolin99@gmail.com>
2016-08-23 21:22:22 +0800

    Signed-off-by: prolin99 <prolin99@gmail.com>

--- ip.py
+++ ip.py
@@ -18,32 +18,32 @@ class My_GUI(Frame):
         self.grid()
         self.createWidgets()
     def createWidgets(self):
-        self.displayText = Label(self)
+        self.displayText = Label(self, width="30" , height="3" )
         self.displayText["text"] = "電腦登記"
-        self.displayText.grid(row=0, column=0, columnspan=5)
+        self.displayText.grid(row=0, column=0 , columnspan=6 )
 
-        self.inputText = Label(self)
+        self.inputText = Label(self , width="30" , height="2" )
         self.inputText["text"] = "IP:" + getmyip()
         self.inputText.grid(row=1, column=0, columnspan=6)
-        self.inputText2 = Label(self)
+        self.inputText2 = Label(self,  width="30" , height="2")
         self.inputText2["text"] = "MAC:" + get_mac_address()
         self.inputText2.grid(row=2, column=0, columnspan=6)
 
-        self.inputText3 = Label(self)
+        self.inputText3 = Label(self, height="2")
         self.inputText3["text"] = "使用者:"
         self.inputText3.grid(row=3, column=0)
         self.inputField = Entry(self)
         self.inputField["width"] = 50
         self.inputField.grid(row=3, column=1, columnspan=5)
 
-        self.inputText4 = Label(self)
+        self.inputText4 = Label(self, height="2")
         self.inputText4["text"] = "位置:"
         self.inputText4.grid(row=4, column=0)
         self.inputField2 = Entry(self)
         self.inputField2["width"] = 50
         self.inputField2.grid(row=4, column=1, columnspan=5)
 
-        self.send = Button(self)
+        self.send = Button(self )
         self.send["text"] = "送出資料"
         self.send.grid(row=5, column=3)
         self.send["command"] =  self.sendMethod
